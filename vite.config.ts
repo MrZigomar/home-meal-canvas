42a87bf383c3d50a7a52bf53971c91d0a1459d95
MrZigomar <166221668+MrZigomar@users.noreply.github.com>
2026-04-02 00:50:24 +0200

    Change base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/home-meal-canvas/", // <--- TRÈS IMPORTANT : C'est le nom de ton dépôt
+  base: "./", // <--- TRÈS IMPORTANT : C'est le nom de ton dépôt
   plugins: [react()],
   resolve: {
     alias: {
