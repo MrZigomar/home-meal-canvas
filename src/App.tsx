1bc5a279c788b69731dc3d07028cff2955eb6280
MrZigomar <guerinot.julien@gmail.com>
2026-04-30 00:07:09 +0200

    add scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import AvanceImmediate from "./pages/AvanceImmediate.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import ScrollToTop from "./components/ScrollToTop.tsx";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <HashRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/avance-immediate" element={<AvanceImmediate />} />
