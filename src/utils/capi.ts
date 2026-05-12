f81a75ae01fbea73cb27cf33401b3737b3bc91ae
MrZigomar <guerinot.julien@gmail.com>
2026-05-13 00:50:49 +0200

    fix capi.ts

--- src/utils/capi.ts
+++ src/utils/capi.ts
@@ -1,4 +1,4 @@
-const WORKER_URL = "https:// meta-capi.guerinot-julien.workers.dev";
+const WORKER_URL = "https://meta-capi.guerinot-julien.workers.dev";
 
 function getCookie(name: string): string | null {
   const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
